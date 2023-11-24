289d7fd5186b075621eb1bca539fd0f613613bd3
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-11-23 22:49:51 -0500

    finished code for final submission

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ A.  We use the following code:
     }
     n++
   }
-  
+
 This yields the following output:
 `)
 n = 500
@@ -127,24 +127,28 @@ while(n<=800){
   n++
 }
 
-// 1.   Write an function that contains an if/else statement for the following requirements:
-// - If student gets 80 or higher: console log  You did a good job
-// - If students get 70 or above: console log Keep trying
-// - If students get 60 or above: console log ehhhh
-// - If students get 55 or above: console log Not to good
-// // - Any grade lower than 55 is Bad Grade But You'll Get Em Next Time
+console.log(`
+ 1.   Write an function that contains an if/else statement for the following requirements:
+ - If student gets 80 or higher: console log  You did a good job
+ - If students get 70 or above: console log Keep trying
+ - If students get 60 or above: console log ehhhh
+ - If students get 55 or above: console log Not to good
+ // - Any grade lower than 55 is Bad Grade But You'll Get Em Next Time
+
+
+Applying the following code:
 
 const gradeComment = (grade) => {
   if (grade >= 80) {
-    console.log(`You did a good job`)
+    console.log('You did a good job')
   } else if (grade >= 70) {
-    console.log(`Keep trying`)
+    console.log('Keep trying')
   } else if (grade >= 60) {
-    console.log(`ehhh`)
+    console.log('ehhh')
   } else if (grade >=55) {
-    console.log(`Not to good`)
+    console.log('Not to good')
   } else {
-    console.log(`Bad Grade But You'll Get Em Next Time`)
+    console.log('Bad Grade But You\'ll Get Em Next Time')
   }
 }
 
@@ -154,7 +158,33 @@ gradeComment(65)
 gradeComment(58)
 gradeComment(23)
 
-// 2.   Write a function that prints out multiples of 10 up to a given input (argument)
+Yield's the following output:
+`)
+
+const gradeComment = (grade) => {
+  if (grade >= 80) {
+    console.log('You did a good job')
+  } else if (grade >= 70) {
+    console.log('Keep trying')
+  } else if (grade >= 60) {
+    console.log('ehhh')
+  } else if (grade >=55) {
+    console.log('Not to good')
+  } else {
+    console.log('Bad Grade But You\'ll Get Em Next Time')
+  }
+}
+
+gradeComment(93)
+gradeComment(72)
+gradeComment(65)
+gradeComment(58)
+gradeComment(23)
+
+console.log(` 2.   Write a function that prints out multiples of 10 up to a given input (argument)
+
+Applying the following code:
+
 const multiplesOfTen = (num) => {
   for (i=10; i<=num; i+=10) {
     console.log(i)
@@ -163,8 +193,45 @@ const multiplesOfTen = (num) => {
 
 multiplesOfTen(320)
 
+Yield's the following output:
+`)
 
-// 3.   Write a function that takes bill amount and item price and says how many quarters they'd get in return
+const multiplesOfTen = (num) => {
+  for (i=10; i<=num; i+=10) {
+    console.log(i)
+  }
+}
+
+multiplesOfTen(320)
+
+console.log(`
+3.   Write a function that takes bill amount and item price and says how many quarters they'd get in return
+
+Applying the following code:
+
+const numOfQuartersReturned = (bill, price) => {
+  // to get the number of quarters returned we first need to get
+  // the total change:
+
+  let change = bill - price
+
+  let coinAmount = (change % 1).toFixed(2)
+
+  // to get the number of quarters we take
+  // the coinAmount and divide it by 0.25
+  // the number of quarters is the quotient
+
+  let numOfQuarters = Math.floor(coinAmount/0.25)
+
+  console.log(\`You receive \${numOfQuarters} quarters in your change\`)
+
+}
+
+numOfQuartersReturned(14, 11)
+
+Yield's the following output:
+
+`)
 
 const numOfQuartersReturned = (bill, price) => {
   // to get the number of quarters returned we first need to get
@@ -186,7 +253,10 @@ const numOfQuartersReturned = (bill, price) => {
 
 numOfQuartersReturned(14, 11)
 
-//4.    Write a function that prints out how many bills you would recieve in change after paying a certain amount.  Return in as large denominations as possible. Parameters should be amount paind and amount cost
+console.log(`
+4.    Write a function that prints out how many bills you would recieve in change after paying a certain amount.  Return in as large denominations as possible. Parameters should be amount paind and amount cost
+
+Applying the following code:
 
 const BillsAndCoinsReturned = (amtPaid, amtCost) => {
   // this is similar to the quarters returned function except this is more involved.
@@ -203,7 +273,13 @@ const BillsAndCoinsReturned = (amtPaid, amtCost) => {
 
   let change = amtPaid - amtCost
 
-  console.log(`You receive $${change.toFixed(2)} as change`)
+  if (change.toFixed(2) != 0) {
+
+    console.log(\`You receive $\${change.toFixed(2)} as change:\`)
+  
+    } else {
+      console.log(\`You receive no change.\`)
+    }
 
   // from our previous function, 
   let coinAmount = (change % 1).toFixed(2)
@@ -219,9 +295,9 @@ const BillsAndCoinsReturned = (amtPaid, amtCost) => {
   // well for 20s, we see what we are left with after taking out the 100s from the change
 
   if(hundreds > 1) {
-    console.log(`You receive ${hundreds} hundred dollar bills`)
+    console.log(\`\${hundreds} hundred dollar bills\`)
   } else if(hundreds > 0) {
-    console.log(`You receive 1 hundred dollar bill`)
+    console.log(\`1 hundred dollar bill\`)
   }
 
   wholeDollarsAmount -= hundreds*100
@@ -229,9 +305,9 @@ const BillsAndCoinsReturned = (amtPaid, amtCost) => {
   let twenties = Math.floor(wholeDollarsAmount/20)
 
   if(twenties > 1) {
-    console.log(`You receive ${twenties} twenty dollar bills`)
+    console.log(\`\${twenties} twenty dollar bills\`)
   } else if(twenties > 0) {
-    console.log(`You receive 1 twenty dollar bill`)
+    console.log(\`1 twenty dollar bill\`)
   }
 
   // for 10s, we see what we are left with after taking out the 100s and the 20s
@@ -240,9 +316,9 @@ const BillsAndCoinsReturned = (amtPaid, amtCost) => {
   let tens = Math.floor(wholeDollarsAmount/10)
 
   if(tens > 1) {
-    console.log(`You receive ${tens} ten dollar bills`)
+    console.log(\`\${tens} ten dollar bills\`)
   } else if(tens > 0) {
-    console.log(`You receive 1 ten dollar bill`)
+    console.log(\`1 ten dollar bill\`)
   }
 
   // and so on...
@@ -252,9 +328,9 @@ const BillsAndCoinsReturned = (amtPaid, amtCost) => {
   let fives = Math.floor(wholeDollarsAmount/5)
 
   if(fives > 1) {
-    console.log(`You receive ${fives} five dollar bills`)
+    console.log(\`\${fives} five dollar bills\`)
   } else if(fives > 0) {
-    console.log(`You receive 1 five dollar bill`)
+    console.log(\`1 five dollar bill\`)
   }
 
   wholeDollarsAmount -= fives*5
@@ -262,9 +338,9 @@ const BillsAndCoinsReturned = (amtPaid, amtCost) => {
   let ones = wholeDollarsAmount
 
   if(ones > 1) {
-    console.log(`You receive ${ones} one dollar bills`)
+    console.log(\`\${ones} one dollar bills\`)
   } else if(ones > 0) {
-    console.log(`You receive 1 one dollar bill`)
+    console.log(\`1 one dollar bill\`)
   }
 
   // and very similar logic for the coins received:
@@ -272,9 +348,9 @@ const BillsAndCoinsReturned = (amtPaid, amtCost) => {
   let quarters = Math.floor(coinAmount/0.25)
 
   if(quarters > 1) {
-    console.log(`You receive ${quarters} quarters`)
+    console.log(\`\${quarters} quarters\`)
   } else if(quarters > 0) {
-    console.log(`You receive 1 quarter`)
+    console.log(\`1 quarter\`)
   }
 
   coinAmount -= quarters*0.25
@@ -282,9 +358,9 @@ const BillsAndCoinsReturned = (amtPaid, amtCost) => {
   let dimes = Math.floor(coinAmount/0.10)
 
   if(dimes > 1) {
-    console.log(`You receive ${dimes} dimes`)
+    console.log(\`\${dimes} dimes\`)
   } else if(dimes > 0) {
-    console.log(`You receive 1 dime`)
+    console.log(\`1 dime\`)
   }
 
   coinAmount -= dimes*0.10
@@ -292,28 +368,182 @@ const BillsAndCoinsReturned = (amtPaid, amtCost) => {
   let nickels = Math.floor(coinAmount/0.05)
 
   if(nickels > 1) {
-    console.log(`You receive ${nickels} nickels`)
+    console.log(\`\${nickels} nickels\`)
   } else if(nickels > 0) {
-    console.log(`You receive 1 nickel`)
+    console.log(\`1 nickel\`)
   }
 
   coinAmount -= nickels*0.05
 
-  let pennies = Math.floor(coinAmount/0.01)
+  let pennies = Math.round(coinAmount/0.01)
 
   if(pennies > 1) {
-    console.log(`You receive ${pennies} pennies`)
+    console.log(\`\${pennies} pennies\`)
   } else if(pennies > 0) {
-    console.log(`You receive 1 penny`)
+    console.log(\`1 penny\`)
   }
-
+  console.log(\`\n\`)
 }
 
 BillsAndCoinsReturned(1.21, 1.21)
+BillsAndCoinsReturned(300, 119.17)
+BillsAndCoinsReturned(250, 201.01)
+
+Yields the following output:
+`)
+
+const BillsAndCoinsReturned = (amtPaid, amtCost) => {
+  // this is similar to the quarters returned function except this is more involved.
+
+  // for bills we have denominations of:
+  // $100, $50, $20, $10, $5, and $1
+  
+  // for coins we have denominations of:
+  // $0.25, $0.10, $0.05, $0.01
+
+  // We are leaving out $2 bills, 50 cent pieces, and dollar coins for simplicity.
+
+  // First we consider $100 bills received, then $20 bills received, and so on down to pennies received.
+
+  let change = amtPaid - amtCost
+
+  if (change.toFixed(2) != 0) {
+
+  console.log(`You receive $${change.toFixed(2)} as change:`)
+
+  } else {
+    console.log(`You receive no change.`)
+  }
+
+  // from our previous function, 
+  let coinAmount = (change % 1).toFixed(2)
+
+  // now we just need to get the wholeDollarsAmount:
+
+  wholeDollarsAmount = Math.round(change - coinAmount)
+
+  // how many 100s do we get back?
+  let hundreds = Math.floor(wholeDollarsAmount/100)
+  // console.log(hundreds)
+  // how many 20s?
+  // well for 20s, we see what we are left with after taking out the 100s from the change
+
+  if(hundreds > 1) {
+    console.log(`${hundreds} hundred dollar bills`)
+  } else if(hundreds > 0) {
+    console.log(`1 hundred dollar bill`)
+  }
+
+  wholeDollarsAmount -= hundreds*100
+
+  let twenties = Math.floor(wholeDollarsAmount/20)
+
+  if(twenties > 1) {
+    console.log(`${twenties} twenty dollar bills`)
+  } else if(twenties > 0) {
+    console.log(`1 twenty dollar bill`)
+  }
+
+  // for 10s, we see what we are left with after taking out the 100s and the 20s
+  wholeDollarsAmount -= twenties*20
+
+  let tens = Math.floor(wholeDollarsAmount/10)
+
+  if(tens > 1) {
+    console.log(`${tens} ten dollar bills`)
+  } else if(tens > 0) {
+    console.log(`1 ten dollar bill`)
+  }
+
+  // and so on...
+
+  wholeDollarsAmount -= tens*10
+
+  let fives = Math.floor(wholeDollarsAmount/5)
+
+  if(fives > 1) {
+    console.log(`${fives} five dollar bills`)
+  } else if(fives > 0) {
+    console.log(`1 five dollar bill`)
+  }
+
+  wholeDollarsAmount -= fives*5
+
+  let ones = wholeDollarsAmount
+
+  if(ones > 1) {
+    console.log(`${ones} one dollar bills`)
+  } else if(ones > 0) {
+    console.log(`1 one dollar bill`)
+  }
+
+  // and very similar logic for the coins received:
+
+  let quarters = Math.floor(coinAmount/0.25)
+
+  if(quarters > 1) {
+    console.log(`${quarters} quarters`)
+  } else if(quarters > 0) {
+    console.log(`1 quarter`)
+  }
+
+  coinAmount -= quarters*0.25
+
+  let dimes = Math.floor(coinAmount/0.10)
+
+  if(dimes > 1) {
+    console.log(`${dimes} dimes`)
+  } else if(dimes > 0) {
+    console.log(`1 dime`)
+  }
+
+  coinAmount -= dimes*0.10
+
+  let nickels = Math.floor(coinAmount/0.05)
+
+  if(nickels > 1) {
+    console.log(`${nickels} nickels`)
+  } else if(nickels > 0) {
+    console.log(`1 nickel`)
+  }
+
+  coinAmount -= nickels*0.05
+
+  let pennies = Math.round(coinAmount/0.01)
+
+  if(pennies > 1) {
+    console.log(`${pennies} pennies`)
+  } else if(pennies > 0) {
+    console.log(`1 penny`)
+  }
+console.log(`\n`)
+}
+
+BillsAndCoinsReturned(1.21, 1.21)
+BillsAndCoinsReturned(300, 119.17)
+BillsAndCoinsReturned(250, 201.01)
 
 
 
-// 5.   Write a program that uses console.log to print all the numbers from 1 to 100 with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+console.log(`
+5.   Write a program that uses console.log to print all the numbers from 1 to 100 with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+Applying the following code:
+
+for (i = 1; i<=100; i++) {
+  if ((i % 3 == 0) && (i % 5 == 0)) {
+    console.log(\`FizzBuzz\`)
+  } else if (i %3 == 0) {
+    console.log(\`Fizz\`)
+  } else if (i%5 == 0) {
+    console.log(\`Buzz\`)
+  } else {
+    console.log(i)
+  }
+}
+
+Yield's the following output:
+`)
 
 for (i = 1; i<=100; i++) {
   if ((i % 3 == 0) && (i % 5 == 0)) {
